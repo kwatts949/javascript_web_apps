1add57a2fe3eea2b5878801fa26d51b4f3df96a0
Kayleigh Watts <kwatts949@gmail.com>
2022-11-21 17:23:48 +0000

    Amend display method to accept user input

--- example-3/messageView.js
+++ example-3/messageView.js
@@ -6,19 +6,23 @@ class MessageView {
       this.displayMessage();
     });
 
-    this.buttonEl = document.querySelector("#hide-message-button");
+    this.hideButtonEl = document.querySelector("#hide-message-button");
 
-    this.buttonEl.addEventListener("click", () => {
+    this.hideButtonEl.addEventListener("click", () => {
       this.hideMessage();
     });
   }
 
   displayMessage() {
     console.log("Thanks for clicking me!");
+    const checkMessage = document.querySelector("#message-input").value;
+
+    console.log(checkMessage);
 
     const message = document.createElement("div");
     message.id = "message";
-    message.innerText = "This message displayed by JavaScript";
+    message.innerText = checkMessage;
+
     document.querySelector("#main-container").append(message);
   }
 
--- example-3/messageView.test.js
+++ example-3/messageView.test.js
@@ -6,15 +6,19 @@ const fs = require("fs");
 const MessageView = require("./messageView");
 
 describe("MessageView", () => {
-  it("clicks the button", () => {
+  it("clicks the button and displays the message", () => {
     document.body.innerHTML = fs.readFileSync("./index.html");
 
     const view = new MessageView();
 
     const buttonEl = document.querySelector("#show-message-button");
+    const inputEl = document.querySelector("#message-input");
+
+    inputEl.value = 'Example text'
     buttonEl.click();
 
     expect(document.querySelector("#message")).not.toBeNull();
+    expect(document.querySelector("#message").innerText).toEqual('Example text');
   });
 
   it("clicks the hide messaage button", () => {
