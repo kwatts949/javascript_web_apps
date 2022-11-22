4e212d548ef2c0e784931a8df049d437ca7230b8
Kayleigh Watts <kwatts949@gmail.com>
2022-11-22 13:57:31 +0000

    Test drive notesClient

---
+++ notes-app/src/notesClient.js
@@ -0,0 +1,11 @@
+class Client {
+  loadNotes(callback) {
+    fetch("http://localhost:3000/notes")
+      .then((response) => response.json())
+      .then((data) => {
+        callback(data);
+      });
+  }
+}
+
+module.exports = Client;
